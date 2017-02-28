45c748c487da24e58f9b1e0a0a89eeff69820682
Maria Vorobyeva <tirimaria@gmail.com>
2017-02-28 17:08:30 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
     var container = document.querySelector('.container');
     var mainContent = document.querySelector('.HolyGrail-body');
     // var hiddenButtonAside = document.querySelector('.is-hidden');
+    var signInButton = document.querySelector('.sign-in');
+    console.log(signInButton);
+    var popUp = document.querySelector('.modal-window');
+    console.log(popUp, 'popUp');
+    var popUpContent = document.querySelector('.modal-window__content');
     var asideButton = document.querySelector('.aside-button');
     var contentButton = document.querySelector('.content-button');
     var sidebarContentDiv = document.querySelector('.aside-content-test');
@@ -18,8 +23,21 @@
         sidebarContentDiv.innerText = window.data.asideContent;
         // hiddenButtonAside.style.display = 'block';
     };
+
+    var showPopUp = function () {
+        var xCoordinate = window.innerWidth * 0.5 + 'px';
+        var yCoordinate = window.innerHeight * 0.5 + 'px';
+        console.log(xCoordinate, yCoordinate);
+        console.log(popUp);
+        popUp.style.display = 'block';
+        popUpContent.innerText = 'something';
+        popUp.style.left = xCoordinate;
+        popUp.style.top = yCoordinate;
+
+    };
     asideButton.addEventListener('click', insertSidebarContent);
     contentButton.addEventListener('click', insertMainContent);
+    signInButton.addEventListener('click', showPopUp);
 
 })();
 
