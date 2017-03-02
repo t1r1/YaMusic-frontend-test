c13d266b05284e3cb2b994e058d38bb1db20e4cf
Maria <tirimaria@gmail.com>
2017-03-02 17:41:00 +0300

    extra task #2 is done

--- js/content-manipulation.js
+++ js/content-manipulation.js
@@ -5,15 +5,25 @@ window.content = (function () {
     var sidebarContentDiv = document.querySelector('.aside-content-test');
     var mainContentDiv = document.querySelector('.main-content-test');
     var asideButton = document.querySelector('.aside-button');
+    var rectBefore = sidebarContentDiv.getBoundingClientRect();
+    console.log(rectBefore);
     var contentButton = document.querySelector('.content-button');
+    var isScrolledIntoView = function (el) {
+        var elemTop = el.getBoundingClientRect().top;
+        var elemBottom = el.getBoundingClientRect().bottom;
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        console.log(isVisible);
+    return isVisible;
+};
     var insertContent = function (evt) {
         var buttonClicked = evt.currentTarget;
         switch (buttonClicked.parentNode) {
             case mainContentDiv:
-                mainContentDiv.innerText = window.data.asideContent; 
+                mainContentDiv.innerText = window.data.longContent; 
+                isScrolledIntoView(sidebarContentDiv);
                 break;
             case sidebar:
-                sidebarContentDiv.innerText = window.data.asideContent;
+                sidebarContentDiv.innerText = window.data.popUpContent;
                 break;
         }; 
     }; 
--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@
 
 (function () {
     var container = document.querySelector('.container');
+    var sidebar = document.querySelector('.sidebar');
+    var sidebarContentDiv = document.querySelector('.aside-content-test');
+    container.addEventListener('scroll', function () {
+        var coordinates = sidebarContentDiv.getBoundingClientRect();
+        if (coordinates.bottom <= 50) {
+            sidebar.classList.add('is-hidden');
+            console.log('Sidebar content is invisible now');
+        } else {
+            sidebar.classList.remove('is-hidden');
+            console.log('Sidebar content is Visible now');
+        }
+    });
     window.utils.setHeight(container);
     window.modalDialog.createDialogFromTemplate();
 })();
